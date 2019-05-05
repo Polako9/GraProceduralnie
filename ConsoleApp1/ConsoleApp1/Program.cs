15db19a1d5d0d140d8694201c09d2366ad8be1a5
Polako93 <48654426+Polako93@users.noreply.github.com>
2019-05-05 19:10:55 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,71 +4,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GraMonolitycznie
+namespace GraProceduralnie
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Wczytuje dane (liczba całkowita) z konsoli. Jeśli 'x' zgłasza wyjątek
+        /// </summary>
+        /// <param name="prompt">Tekst zachęty wyświetlany uzytkownikowi</param>
+        /// <returns>liczba odczytana ze standardowego wejscia</returns>
+        /// <exception cref="InvalidOperationException">gdy użytkownik poda znak X</exception>
+        private static int WczytajDane(string prompt = "Podaj liczbę lub X jeśli koniec: ")
         {
-            /*
-            Console.WriteLine("Witaj!");
-            Console.Write("Podaj swoje imię: ");
-            string x = Console.ReadLine();
-            Console.WriteLine($"Witaj, {x}");
-            */
-
-            // 1. Komputer losuje liczbę
-            Random generator = new Random();
-            int wylosowana = generator.Next(1, 101);
-            Console.WriteLine("Wylosowałem liczbę od 1 do 100. \n Odgadnij ją");
-
-#if(DEBUG)
-            Console.WriteLine(wylosowana);
-#endif
-
-            //wykonuj
-            bool trafiono = false; //wartownik (zwany czasami flagą)
+            int propozycja = 0;
             do
             {
-                #region Krok 2. Człowiek proponuje rozwiązanie
-                Console.Write("Podaj swoją propozycję: ");
+                Console.Write(prompt);
                 string tekst = Console.ReadLine();
                 if (tekst.ToLower() == "x")
-                    break;
-
-                int propozycja = 0;
+                    throw new InvalidOperationException();
                 try
                 {
-                    propozycja = Convert.ToInt32(tekst);
+                    propozycja = Convert.ToInt32(tekst); // int.Parse(tekst)
+                    break;
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Nie podano liczby!");
-                    continue;
+                    Console.WriteLine("Nie podano liczby! Spróbuj jeszcze raz.");
                 }
                 catch (OverflowException)
                 {
-                    Console.WriteLine("Liczba nie mieści się w rejestrze!");
-                    continue;
+                    Console.WriteLine("Liczba nie mieści się w rejestrze! Spróbuj jeszcze raz.");
+                }
+            }
+            while (true);
+
+            return propozycja;
+        }
+
+        private static int Losuj(int min = 1, int max = 100)
+        {
+            Random generator = new Random();
+            int wylosowana = generator.Next(min, max + 1);
+            return wylosowana;
+        }
+
+        private static int wylosowana = 0;
+
+        private static string Ocena(int propozycja)
+        {
+            if (propozycja < wylosowana)
+                return "za mało";
+            else if (propozycja > wylosowana)
+                return "za dużo";
+            else
+            {
+                return "trafiono";
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // 1. Komputer losuje liczbę
+            wylosowana = Losuj(); // może być zgłoszony wyjatek, gdy min > max
+            Console.WriteLine("Wylosowałem liczbę od 1 do 100. \n Odgadnij ją");
+
+#if(DEBUG)
+            Console.WriteLine("zgaduj xd");
+#endif
+
+            //wykonuj
+            int propozycja = 0;
+            do
+            {
+                try
+                {
+                    propozycja = WczytajDane("Podaj swoją propozycję (X - gdy koniec)");
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("Szkoda, że kończymy. Do widzenia.");
+                    return;
                 }
 
                 Console.WriteLine($"Przyjąłem wartość {propozycja}");
-                #endregion
 
-
-                #region Krok 3. Komputer ocenia propozycję
-                if (propozycja < wylosowana)
-                    Console.WriteLine("za mało");
-                else if (propozycja > wylosowana)
-                    Console.WriteLine("za dużo");
-                else
-                {
-                    Console.WriteLine("trafiono");
-                    trafiono = true;
-                }
-                #endregion
+                string ocena = Ocena(propozycja);
+                Console.WriteLine(ocena);
+                if (ocena == "trafiono")
+                    break;
             }
-            while (!trafiono);
+            while (true);
             //do momentu trafienia
 
             Console.WriteLine("Koniec gry");
